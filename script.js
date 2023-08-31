1dfd3de8b3d556c2bf0231c85e2a1906a829b002
renata-aline <renatalinec@gmail.com>
2023-08-31 14:22:16 -0300

    trocando ícone do botao

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
 const startPauseBt = document.querySelector('#start-pause');
 const musicaFocoInput = document.querySelector('#alternar-musica');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const iniciarOuPausarBtIcone = document.querySelector('.app__card-primary-butto-icon');
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
 const audioPlay = new Audio('/sons/play.wav');
 const audioPausa = new Audio('/sons/pause.mp3');
@@ -69,9 +71,9 @@ function alterarContexto(contexto) {
 
 const contagemRegressiva = () => {
     if(tempoDecorridoEmsegundos <= 0){
-        audioTempoFinalizado.play()
-      zerar();  
-      alert('tempo finalizado!') 
+        audioTempoFinalizado.play()  
+      alert('tempo finalizado!');
+      zerar(); 
       return 
     }
     tempoDecorridoEmsegundos -= 1
@@ -89,9 +91,13 @@ function iniciarOuPausar(){
     }
     audioPlay.play();
     intervaloId = setInterval(contagemRegressiva, 1000);
+    iniciarOuPausarBt.textContent = "Pausar";
+    iniciarOuPausarBtIcone.setAttribute('src',`/imagens/pause.png`);
 }
 
 function zerar(){
     clearInterval(intervaloId);
+    iniciarOuPausarBt.textContent = "começar"
+    iniciarOuPausarBtIcone.setAttribute('src',`/imagens/play_arrow.png`);
     intervaloId = null;
 }
